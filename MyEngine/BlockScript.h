07b52a5f9a51a33e6c2ee3f311b8dcb7edc6512c
이윤상 <64140710+passion1ee@users.noreply.github.com>
2024-05-19 01:45:38 +0900

    .

---
+++ MyEngine/BlockScript.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ysScript.h"
+using namespace ys;
+class BlockScript : public ys::Script
+{
+public:
+	BlockScript();
+	~BlockScript();
+
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void LateUpdate() override;
+	virtual void Render(HDC hDC) override;
+
+	virtual void OnCollisionEnter(Collider * other) override;
+	virtual void OnCollisionStay(Collider* other) override;
+	virtual void OnCollisionExit(Collider* other) override;
+
+private:
+	float deathTime;
+	float time;
+};
+
+
---
+++ MyEngine/CameraScript.cpp
@@ -0,0 +1,32 @@
+#include "CameraScript.h"
+#include <ysCamera.h>
+#include <ysTransform.h>
+
+namespace ys
+{
+	ys::CameraScript::CameraScript()
+	{
+	}
+
+	ys::CameraScript::~CameraScript()
+	{
+	}
+
+	void ys::CameraScript::Init()
+	{
+	}
+
+	void ys::CameraScript::Update()
+	{
+
+	}
+
+	void ys::CameraScript::LateUpdate()
+	{
+	}
+
+	void ys::CameraScript::Render(HDC hDC)
+	{
+	}
+}
+
